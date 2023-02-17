69390b0d3a8ce79aadb9dc5e111834f7cb841451
Paul Beaudoin <contact@paulbeaudoin.com>
2023-02-17 15:56:23 -0500

    Change create_test_data script into a class

--- create_test_data.py
+++ create_test_data.py
@@ -24,68 +24,60 @@ ITEMS = [
     (10002, 102, 'maf92')
 ]
 
-sierra_client = None
 
+class CreateTestData:
 
-def connect():
-    global sierra_client
+    def connect(self):
+        load_env_file(os.environ['ENVIRONMENT'], 'config/development.yaml')
+        self.sierra_client = PostgreSQLClient(
+            '127.0.0.1',
+            os.environ['SIERRA_DB_PORT'],
+            os.environ['SIERRA_DB_NAME'],
+            os.environ['SIERRA_DB_USER'],
+            os.environ['SIERRA_DB_PASSWORD']
+        )
+        self.sierra_client.connect()
 
-    load_env_file(os.environ['ENVIRONMENT'], 'config/development.yaml')
-    sierra_client = PostgreSQLClient(
-        '127.0.0.1',
-        os.environ['SIERRA_DB_PORT'],
-        os.environ['SIERRA_DB_NAME'],
-        os.environ['SIERRA_DB_USER'],
-        os.environ['SIERRA_DB_PASSWORD']
-    )
-    sierra_client.connect()
+    def disconnect(self):
+        self.sierra_client.close_connection()
 
+    def insert_data(self, table, row):
+        values_str = ', '.join(['%s' for n in range(len(row))])
+        query = 'INSERT INTO sierra_view.{} VALUES ({})'.format(
+            table, values_str)
+        print(' => {} < {}'.format(query, row))
+        self.db_query(query, row)
 
-def disconnect():
-    global sierra_client
-    sierra_client.close_connection()
+    def db_query(self, query, data=None):
+        with self.sierra_client.pool.connection() as conn:
+            try:
+                conn.execute(query, data)
+            except Exception as e:
+                conn.rollback()
+                print(
+                    ('Error executing database query \'{query}\': '
+                     '{error}').format(query=query, error=e))
+                raise PostgreSQLClientError(
+                    ('Error executing database query \'{query}\': '
+                     '{error}').format(query=query, error=e)) from None
 
+    def create_test_data(self):
+        self.connect()
 
-def insert_data(table, row):
-    global sierra_client
+        print('Truncating tables')
+        for table in ['hold', 'item_view']:
+            self.db_query('TRUNCATE TABLE sierra_view.{}'.format(table))
 
-    values_str = ', '.join(['%s' for n in range(len(row))])
-    query = 'INSERT INTO sierra_view.{} VALUES ({})'.format(table, values_str)
-    print(' => {} < {}'.format(query, row))
-    db_query(query, row)
+        print('Filling tables')
+        for row in ITEMS:
+            self.insert_data('item_view', row)
+        for row in HOLDS:
+            current_date = datetime.datetime.utcnow().isoformat().split('T')[0]
+            row[3] = row[3].replace('YYYY-MM-DD', current_date)
+            self.insert_data('hold', row)
 
-
-def db_query(query, data=None):
-    with sierra_client.pool.connection() as conn:
-        try:
-            conn.execute(query, data)
-        except Exception as e:
-            conn.rollback()
-            print(
-                ('Error executing database query \'{query}\': '
-                 '{error}').format(query=query, error=e))
-            raise PostgreSQLClientError(
-                ('Error executing database query \'{query}\': '
-                 '{error}').format(query=query, error=e)) from None
-
-
-def create_test_data():
-    connect()
-
-    print('Truncating tables')
-    for table in ['hold', 'item_view']:
-        db_query('TRUNCATE TABLE sierra_view.{}'.format(table))
-
-    print('Filling tables')
-    for row in ITEMS:
-        insert_data('item_view', row)
-    for row in HOLDS:
-        current_date = datetime.datetime.utcnow().isoformat().split('T')[0]
-        row[3] = row[3].replace('YYYY-MM-DD', current_date)
-        insert_data('hold', row)
-
-    disconnect()
+        self.disconnect()
 
 
 if __name__ == '__main__':
-    create_test_data()
+    CreateTestData().create_test_data()
